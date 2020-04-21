6efc4308310cb635879901e3da00ae3897aee336
stzups <lemonpickles1558@gmail.com>
2020-04-21 17:30:49 -0400

    add timer for reading file to buffer

--- Compressor/Compressor.cpp
+++ Compressor/Compressor.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[]) 
 {
+    int START_COMPARES = 2;
+    int COMPARES = 2;
+
     if (argc > 0) 
     {
         cout << "Opening " << argv[argc - 1] << endl;
-        ifstream open(argv[argc - 1]);
-        if (open) 
+        ifstream file(argv[argc - 1], ios::binary | ios::ate);
+        if (file) 
         {
-            string str = argv[argc - 1];
-            cout << "Opened " << str.substr(str.find_last_of("\\") + 1) << endl;
+            string filen = argv[argc - 1];
+            cout << "Opened " << filen.substr(filen.find_last_of("\\") + 1) << endl;
+            clock_t time = clock();
+            streamsize size = file.tellg();
+            file.seekg(0, ios::beg);
+
+            vector<char> buffer(size);
+            if (file.read(buffer.data(), size))
+            {
+                cout << "Read in " << ((float)(clock() - time) / CLOCKS_PER_SEC * 1000) << "ms" << endl;
+
+            }
+            else
+            {
+                cout << "Error opening " << filen;
+                return 1;
+            }
         }
         else
         {
             cout << "Error opening " << argv[argc - 1] << endl;
+            return 1;
         }
     }
     else 
     {
         cout << "Not enough arguments - please specify a file to open" << endl;
     }
+
+    return 0;
 }
